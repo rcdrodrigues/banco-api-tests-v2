9d8e6c26b0930be752b29b3dc243859a6be4bfa9
Ricardo Rodrigues <originalrcd@hotmail.com>
2025-10-23 06:53:58 -0300

    refatoração login/ transferencias

--- test/login.test.js
+++ test/login.test.js
@@ -1,16 +1,17 @@
 const request = require('supertest');
 const {expect} = require('chai');
+require('dotenv').config();
+const postLogin = require ('../fixtures/postLogin.json')
 
 describe('Login', () => {
     describe('POST /login', () => {
+        const bodyLogin = { ...postLogin }
+
         it ('Deve retornar 200 com um token em string quando usar credencial válidas', async () => {
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request (process.env.BASE_URL) 
                 .post('/login')
                 .set('Content-Type','application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                  })
+                .send(bodyLogin)
         
         //console.log(resposta.status,resposta.body)
         expect(resposta.status).to.equal(200);          
