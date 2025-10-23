94590c082b62ed952e93ca6838a87b8a69585271
Ricardo Rodrigues <originalrcd@hotmail.com>
2025-10-23 06:34:43 -0300

    Restaura versão otimizada de transferencia.test.js do stash tmp-sync

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,16 +1,23 @@
 const request = require('supertest');
-const {expect} = require('chai');
+const {expect} = require ('chai');
+require('dotenv').config();
+const {obterToken} = require ('../helpers/autenticacao')
 
 describe('Transferências', () => {
-    describe('POST/ transferencias', () => {
+    let token 
+
+   
+    beforeEach ( async () => {
+         token =  await obterToken ('julio.lima', '123456')
+    });
+
+    
+
         it ('Deve retornar sucesso com 201, quando o valor da transferência for igual ou acima de R$ 10.00', async () => {
-            const respostaLogin = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type','application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-        })
+                .send{bodyTransferencia} 
 
             const token = respostaLogin.body.token
 
